22848a0ff41c648980367e8220106c859bc28968
Timothé HOFMANN <timothe@logomotion.fr>
2021-10-25 15:41:34 +0200

    Login page

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    connected: false,
   },
   mutations: {
+    CONNECTION(state) {
+      state.connected = true;
+    },
+    DISCONNECTION(state) {
+      state.connected = false;
+    },
   },
   actions: {
+    login(context, playload) {
+      if(playload) 
+        context.commit('CONNECTION')
+      else
+        context.commit('DISCONNECTION')
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
